29325b35870fd57f11e9e0e889299362ab6a5be9
atkinsjeff <jwa5cz@virginia.edu>
2016-10-20 13:40:57 -0400

    updating code after a pointless week and am no more ahead. Code cleaned though and has an export function of some kind

--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
+###functions.R for LaserquestR project focused on calculating CSC metrics for LiDAR data
 
 
-#
 #Libraries
 library(plyr)
 library(dplyr)
@@ -45,21 +45,24 @@ code_hits <- function(df) {
 
 adjust_by_user <- function(df, user.ht) {
      df$return_distance <- df$return_distance + user.ht
-     return(df)
+     df
 }
 # this function mirrors the read.table, read.csv function, but is written for pcl data
 
-pcl.diagnostic.plot <- function(df, site, min.height) {
+pcl.diagnostic.plot <- function(df, filename) {
      plot(df$index,
           df$return_distance,
-          main = as.character(site),
-          ylim = c(min.height, 40),
+          main = filename,
+          
           ylab = "Canopy Height (m)",
-          xlab = "Index Value")
-     
+          xlab = "")
 }
 
-
+get.transect.length <- function (df) {
+     
+     transect.length <- (length(which((df$return_distance <= -9999))) - 1) * 10
+     
+}
 ##########################################
 ##########################################
 # Function to read in a pcl text file and
@@ -133,7 +136,7 @@ split_transects_from_pcl <- function(pcl_data, transect.length, marker.distance,
      #### this needs to be adjusted to account for smaller transects
      # a vector of integer categories for each "chunk" within each segment
      # This should go from 1-10 and be spaced evenly in "index" space
-#for (i in 1:(max(pcl_data$seg_num) - 1)) {
+for (i in 1:(max(pcl_data$seg_num) - 1)) {
       for (i in 1:(max(pcl_data$seg_num))) {
           this_segment <- subset(pcl_data, pcl_data$seg_num == i)
           this_segment$chunk_num <- cut(this_segment$index, 10, labels = FALSE)
@@ -142,9 +145,8 @@ split_transects_from_pcl <- function(pcl_data, transect.length, marker.distance,
      
      # Make sure we didn't make too many chunks in any segment
      stopifnot(max(results$chunk_num) < 11)
-     
-     #stopifnot(max(results$seg_num) < ((transect.length/marker.distance) + 1))
-#}
+     stopifnot(max(results$seg_num) < ((transect.length/marker.distance) + 1))
+}
      # Code segment to create zbin and xbin
      results$xbin <- ((results$seg_num * 10) - 10)  + results$chunk_num
      results$zbin <- ceiling(results$return_distance)
@@ -156,9 +158,47 @@ split_transects_from_pcl <- function(pcl_data, transect.length, marker.distance,
      # Write out if write parameter is set at top
      if (write_out) write.csv(results, paste0(data_dir, output_file_name, ".with_categories.csv"), row.names = FALSE)
      
+     results <- distinct(results, index, .keep_all = TRUE)
      results
 }
 
+
+#####Canopy metrics before matrix creations
+
+csc_metrics <- function(df, filename) {
+     z <- df
+     z <- subset(z, return_distance >= 0)
+     
+     
+     mean.return.ht = mean(z$return_distance, na.rm = TRUE)
+     message("Mean Return Height (m) -- meanHeight in old code")
+     print(mean.return.ht)
+     
+     sd.ht = sd(z$return_distance, na.rm = TRUE)
+     message("Standard Deviation of Canopy Height returns-- meanStd in old code")
+     print(sd.ht)
+     
+     sky.fraction = (1 - (length(which(df$can_hit == TRUE)) / length(df$return_distance))) * 100
+     message("Sky Fraction (%)")
+     print(sky.fraction)
+     
+     cover.fraction = 100 - sky.fraction
+     message("Cover Fraction (%)")
+     print(cover.fraction)
+     
+     max.ht = max(df$return_distance, na.rm = TRUE)
+     message("Max Measured Canopy Height (%)")
+     print(max.ht)
+     
+     csc.variable.list <- list(plot = filename,
+                               mean.return.ht = mean.return.ht,
+                               sd.return.ht = sd.ht,
+                               sky.fraction = sky.fraction,
+                               cover.fraction = cover.fraction,
+                               max.ht = max.ht)
+     csc.variable.list <- data.frame(csc.variable.list)
+     return(csc.variable.list)
+}
 
 ##########################################
 ##########################################
@@ -175,20 +215,22 @@ make_matrix_part_one <- function(df) {
      #and it should go from x 1:40 and z to whatever so there are empty values in there
      z = df
      z <- subset(z, return_distance >= 0)
-     zz <- setNames(aggregate(return_distance ~ xbin, data = z, FUN = mean), c("xbin", "mean.ht"))
-     zzz <-setNames(aggregate(return_distance ~ xbin, data = z, FUN = sd), c("xbin", "sd.ht"))
-     zzzz <- setNames(aggregate(return_distance ~ xbin, data = z, FUN = max), c("xbin", "max.ht"))
+     # zz <- setNames(aggregate(return_distance ~ xbin, data = z, FUN = mean), c("xbin", "mean.ht"))
+     # zzz <-setNames(aggregate(return_distance ~ xbin, data = z, FUN = sd), c("xbin", "sd.ht"))
+     # zzzz <- setNames(aggregate(return_distance ~ xbin, data = z, FUN = max), c("xbin", "max.ht"))
      l <- setNames(aggregate(index ~ xbin, data = df, FUN = length), c("xbin", "lidar.pulses"))
-     m <- setNames(aggregate(return_distance ~ xbin + zbin, data = df, FUN = length), c("xbin", "zbin","bin.hits")) 
+     m <- setNames(aggregate(return_distance ~ xbin + zbin, data = df, FUN = length), c("xbin", "zbin","bin.hits"))
      m <- m[!m$zbin < 0, ]
      n <- setNames(aggregate(sky_hit ~ xbin, data = df, FUN = sum), c("xbin", "sky.hits"))
      k <- setNames(aggregate(can_hit ~ xbin, data = df, FUN = sum), c("xbin", "can.hits"))
-     p <- merge(l, m, by = c("xbin"), all = TRUE)
-     p <- merge(p, n, by = c("xbin"), all = TRUE)
-     p <- merge(p, k, by = c("xbin"), all = TRUE)
-     p <- merge(p, zz, by = c("xbin"), all = TRUE)
-     p <- merge(p, zzz, by = c("xbin"), all = TRUE)
-     p <- merge(p, zzzz, by = c("xbin"), all = TRUE)
+     
+     p <- Reduce(function(x, y) merge(x,y, all = TRUE), list(m, l, n, k))
+     # p <- merge(l, m, by = c("xbin"), all = TRUE)
+     # p <- merge(p, n, by = c("xbin"), all = TRUE)
+     # p <- merge(p, k, by = c("xbin"), all = TRUE)
+     # p <- merge(p, zz, by = c("xbin"), all = TRUE)
+     # p <- merge(p, zzz, by = c("xbin"), all = TRUE)
+     # p <- merge(p, zzzz, by = c("xbin"), all = TRUE)
      replace(p, is.na(p), 0)#This will correct for any gaps w/out msmts as all NAs will be 0
  
 } 
@@ -218,86 +260,66 @@ make_matrix <- function(df) {
      return(df)
 }
 
-#    
-# make_sky <- function(df) {
-#      aggregate(sky_hit ~ xbin, data = df, FUN = sum)     
-# }
-# 
-# make_can <- function(df) {
-#      aggregate(can_hit ~ xbin, data = df, FUN = sum)     
-# }
-# 
-# just_the_hits <- function(df) {
-#      p <- make_sky(df)
-#      q <- make_can(df)
-#      merge(p, q)
-# }
-
-
-
 
 #####this series of functions creates VAI
 calc_vai <- function(df) {
      
-     df$vai <- (df$bin.hits / df$lidar.pulses) 
-     df$vai <- df$vai * 8 #adjust for max lai?
-     df$vai <- df$vai * -1
-     df$vai <- log(1.0 - df$vai*0.9817)/0.5
+     df$cover.fraction <- (df$bin.hits / df$lidar.pulses) 
+      #adjust for max lai?
+     # cover.one <- which(df$vai != 0)
+     
+     df$vai <- (log(1.0 - df$cover.fraction*0.9817)  * -1) /0.5
      df[is.na(df)] <- 0
      return(df)
  
 }
 
-max_vai <- function(df) {
-     z = df
-     max.vai <- aggregate(vai ~ xbin, data = z, FUN = max)
-     p <- merge(df, z, by = c("xbin"), all = TRUE)
+#############################
+#############################
+#############################
+## Need to make a summary matrix now based on each column
+
+make_summary_matrix <- function(df, m) {
+     # df$xbin <- as.factor(df$xbin)
+     df <- subset(df, return_distance > 0)
      
-}
-
-bin_vai <- function(df) {
-     df$vai <- calc_vai(df)
-}
-
-calc_sum_vai <- function(df) {
-
-          z <- setNames(aggregate(vai ~ xbin, data = df, FUN = sum), c("xbin", "sum.vai.by.xbin")) #calculates the sum of VAI by each column
-          merge(df, z, by = c("xbin"), all = TRUE)
-                
-}
-
-
-
-
-### this section of functions create the intermediates to calculate complexity metrics
-calc_vai_z <- function(df) {
-     df$vai_z <- df$vai * df$zbin
-     #print(df$vai_z)
-     z <- setNames(aggregate(vai_z ~ xbin, data = df, FUN = sum), c("xbin", "mean.leaf.ht.unadj"))
-     merge(df, z, by = c("xbin"), all = TRUE)
+     a <- setNames(aggregate(return_distance ~ xbin, data = df, FUN = mean, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "mean.ht"))
      
-}
-
-calc_bin_ht <- function(df) {
-     df$mean.leaf.ht.by.xbin <- df$mean.leaf.ht.unadj / df$sum.vai.by.xbin
-     return(df)
-}
-
-calc_mean_leaf_ht <- function(df) {
-     df <- calc_sum_vai(df)
-     df <- calc_vai_z(df)
-     df <- calc_bin_ht(df)
-}
-
-calc_std_bin <- function(df) {
      
-     df$std.bin.pre <- df$vai * ( (df$zbin - df$mean.leaf.ht.by.xbin)^2)
-     z <- setNames(aggregate(std.bin.pre ~ xbin, data = df, FUN = sum), c("xbin", "std.bin.num"))
-     df <- merge(df, z, by = c("xbin"), all = TRUE)
-     df$std.bin <- df$std.bin.num / df$sum.vai.by.xbin
-     return(df)
+     b <- setNames(aggregate(return_distance ~ xbin, data = df, FUN = sd, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "sd.ht"))
      
+     c <- setNames(aggregate(return_distance ~ xbin, data = df, FUN = max, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "max.ht"))
+     
+     d <- setNames(aggregate(vai ~ xbin, data = m, FUN = max, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "max.vai"))
+     e <- setNames(aggregate(vai ~ xbin, data = m, FUN = sum, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "sum.vai"))
+     f <- setNames(aggregate(vai ~ xbin, data = m, FUN = sd, na.rm = FALSE, na.action = 'na.pass'), c("xbin", "sd.vai"))
+     
+     # this is height at which max vai occurs
+     g <- m$zbin[match(d$max.vai, m$vai)]  
+     g <- data.frame(g)
+     colnames(g) <- c("max.vai.z")
+     
+     #mean column leaf height that is the "heightBin" from Matlab code
+     m$vai.z <- m$vai * m$zbin
+     h <- setNames(aggregate(vai.z ~ xbin, data = m, FUN = sum, na.rm = FALSE,  na.action = 'na.pass'), c("xbin", "vai.z.sum"))
+           
+  
+     # this section joins all these guys together
+     p <- join_all(list(a, b, c, d, e, f, h), by = "xbin", type = "full")
+     p <- cbind(p, g)
+
+    
+     
+     #need to fix the missing first here
+     p$mean.ht[is.na(p$mean.ht)] <- 0
+     p$sd.ht[is.na(p$sd.ht)] <- 0
+     p$max.ht[is.na(p$max.ht)] <- 0
+      
+     p$height.bin <- p$vai.z.sum / p$sum.vai
+     
+     return(p)
 }
+
 
 ##########################
 ##########################
@@ -305,29 +327,143 @@ calc_std_bin <- function(df) {
 # 
 
 # RUGOSITY
-calc_rugosity <- function(df) {
-     df$stdstd <- df$std.bin * df$std.bin
-     stdstd = sum(df$stdstd, na.rm = TRUE) 
-     stdstd = stdstd / max(df$zbin)
-     stdstd = stdstd / max(df$xbin)
+calc_rugosity <- function(df, m, filename) {
      
-     message("Square of leaf height variance (stdStd from old script)")
-     print(stdstd)
+     a <- subset(df, max.vai.z > 0)
+     
+     mean.height = mean(df$mean.ht)
+     message("MeanHeight")
+     print(mean.height)
+     
+     transect.length = max(df$xbin)
+     message("Transect Length (m)")
+     print(transect.length)
+     
+     mode.el = mean(df$max.vai.z)
+     message("Mean Height of Maximum Return Density -- modeEl")
+     print(mode.el)
+     
+     height.2 <- sd(df$mean.ht)
+     message("Standard Deviation of mean height for each xbin - height2")
+     print(height.2)
+     
+     max.el = max(df$max.vai.z)
+     message("Maximum VAI for entire transect -- max el!")
+     print(max.el)
+     
+     b <- subset(df, max.vai > 0)
+     b$max.vai.sq <- b$max.vai^2
+     mode.2 <- mean(b$max.vai.sq)
+     message("Mean height of squared max VAI whatever the hell that is -- or mode2")
+     print(mode.2)
+     
+     max.can.ht = max(df$max.ht)
+     message("Max canopy height (m)")
+     print(max.can.ht)
+     
+    
+     
+     mean.max.ht = mean(df$max.ht)
+     message("Mean Max canopy height (m) -- meanTopel w/ deep gaps removed")
+     print(mean.max.ht)
      
      
-     meanstd = ((sum(df$std.bin, na.rm = TRUE) ) /max(df$zbin)) / max(df$xbin)
-     
-     message("Mean Standard deviation of leaf heights")
-     print(meanstd)
+     mean.vai = mean(df$sum.vai)
+     message("Mean VAI")
+     print(mean.vai)
      
      message("Maximum VAI")
-     print(max(df$vai))
+     max.vai = max(df$sum.vai)
+     print(max.vai)
      
-     rugosity = (stdstd - meanstd * meanstd)^0.5 
+     e <- subset(df, max.ht == 0)
+     deep.gaps <- nrow(e)
+     message("Deep Gaps")
+     print(deep.gaps)
+     
+     porosity = deep.gaps/transect.length
+     message("Bin porosity")
+     print(porosity)
+     
+     #being rugosity intermediates
+      
+     #first we adjust the vai at each x,z by the z height of the bin
+     combo.meal <- merge(df, m, by = "xbin")
+     
+     combo.meal$std.bin.num <- combo.meal$vai * ((combo.meal$zbin  - combo.meal$height.bin)^2)
+     j <- aggregate(std.bin.num ~ xbin, data = combo.meal, FUN = sum, na.rm = FALSE, na.action = 'na.pass')
+     j[is.na(j)] <- 0
+     
+     super.size <- merge(df, j, by = "xbin")
+     super.size$std.bin <- super.size$std.bin.num / super.size$sum.vai
+     
+
+     super.size$std.std.pre <- (super.size$std.bin * super.size$std.bin) / transect.length
+     super.size$std.std.pre[is.na(super.size$std.std.pre)] <- 0
+     std.std <- sum(super.size$std.std.pre)
+     # super.size$std.std.pre[is.infinite(super.size$std.std.pre)] <- 0
+
+     super.size$mean.std.pre <- (super.size$std.bin / transect.length)
+     super.size$mean.std.pre[is.na(super.size$mean.std.pre)] <- 0
+     
+     # super.size$mean.std.pre[is.infinite(super.size$mean.std.pre)] <- 0
+     mean.std = sum(super.size$mean.std.pre)
+
+     message("Square of leaf height variance (stdStd from old script)")
+     print(std.std)
+     
+ 
+     message("Mean Standard deviation of leaf heights -- meanStd")
+     print(mean.std)
+    
+     rugosity = (std.std - mean.std * mean.std)^0.5
      message("Canopy Rugosity")
      print(rugosity)
+     
+     # #uses temp. data frame with deep gaps removed
+     
+     jess.rugosity = sd(df$max.ht)
+     
+     
+     message("Surface Rugosity--TopRugosity")
+     print(jess.rugosity)
+     
+     #Rumple is the ratio of top rugoisty to ground area.
+     rumple = jess.rugosity / transect.length
+     message("Rumple--surface rugosity/ground area")
+     print(rumple)
+     
+     variable.list <- list(plot = filename,
+                           mean.height = mean.height,
+                           transect.length = transect.length, 
+                           mode.el = mode.el, 
+                           height.2 = height.2, 
+                           max.el = max.el,
+                           mode.2 = mode.2,
+                           max.can.ht = max.can.ht,
+                           mean.max.ht = mean.max.ht,
+                           mean.vai = mean.vai,
+                           max.vai = max.vai,
+                           deep.gaps = deep.gaps,
+                           porosity = porosity,
+                           std.std = std.std,
+                           mean.std = mean.std,
+                           rugosity = rugosity,
+                           top.rugosity = jess.rugosity,
+                           rumple = rumple)
+    
+     
+     #now to write to csv
+     variable.list <- data.frame(variable.list)
+     return(variable.list)
+     
 }
 
+write.pcl.to.csv <- function(variable.list, filename) {
+     
+     filename2 <- paste(filename, ".csv", sep="")
+     write.csv(variable.list, file = filename2)
+}
 
      
      
@@ -336,23 +472,3 @@ calc_rugosity <- function(df) {
  
 
 
-calc_rugosity_jess <- function(df) {
-     p <- aggregate(max.ht ~ xbin, data = df, FUN = sd)
-     p
-     
-}
-# 
-# 
-# 
-# summarize_categorized_pcl <- function(pcl_data) {
-#      
-#      # Aggregate a categorized pcl transect and calculate mean, sd, and count for each chunk of each segment
-#      aggregated_result <- aggregate(cbind(return_distance, intensity) ~ seg_num + chunk_num, data = pcl_data, FUN = function(x) c(mean = mean(x), sd = sd(x))) 
-#      aggregated_result[order(aggregated_result$seg_num), ]
-# }
-# 
-# 
-# # Do all the things
-# summarize_categorized_pcl(split_transects_from_pcl(read_and_check_pcl(data_dir, filename)))
-# 
-# 
